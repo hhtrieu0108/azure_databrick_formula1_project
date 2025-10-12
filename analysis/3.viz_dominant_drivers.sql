db9e1b5073f8913cf285e70e40b6a3b76248a933
Huynh Trieu <162791024+hhtrieu0108@users.noreply.github.com>
2025-10-12 15:28:51 +0700

    Update 3.viz_dominant_drivers.sql

--- analysis/3.viz_dominant_drivers.sql
+++ analysis/3.viz_dominant_drivers.sql
@@ -5,26 +5,31 @@
 
 -- COMMAND ----------
 
-CREATE OR REPLACE TEMP VIEW v_dominant_drivers
-AS
-SELECT driver_name,
+CREATE OR REPLACE TEMP VIEW v_dominant_drivers AS
+SELECT 
+       driver_name,
        COUNT(1) AS total_races,
        SUM(calculated_points) AS total_points,
        AVG(calculated_points) AS avg_points,
        RANK() OVER(ORDER BY AVG(calculated_points) DESC) driver_rank
-  FROM f1_presentation.calculated_race_results
+FROM f1_presentation.calculated_race_results
 GROUP BY driver_name
 HAVING total_races >= 50
 ORDER BY avg_points DESC;
 
 -- COMMAND ----------
 
-SELECT race_year,
+SELECT 
+       race_year,
        driver_name,
        COUNT(1) AS total_races,
        SUM(calculated_points) AS total_points,
        AVG(calculated_points) AS avg_points
-  FROM f1_presentation.calculated_race_results
-  WHERE driver_name IN (SELECT driver_name FROM v_dominant_drivers WHERE driver_rank <= 10)
-GROUP BY race_year,driver_name
+FROM f1_presentation.calculated_race_results
+WHERE driver_name IN (
+       SELECT driver_name
+       FROM v_dominant_drivers
+       WHERE driver_rank <= 10
+)
+GROUP BY race_year, driver_name
 ORDER BY race_year, avg_points DESC;
