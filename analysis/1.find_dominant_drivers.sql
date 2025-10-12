a10051978465e992407b8e127c91a1d4c40e1dad
Huynh Trieu <162791024+hhtrieu0108@users.noreply.github.com>
2025-10-12 15:27:41 +0700

    Update 1.find_dominant_drivers.sql

--- analysis/1.find_dominant_drivers.sql
+++ analysis/1.find_dominant_drivers.sql
@@ -1,20 +1,22 @@
 -- Databricks notebook source
-SELECT driver_name,
+SELECT
+       driver_name,
        COUNT(1) AS total_races,
        SUM(calculated_points) AS total_points,
        AVG(calculated_points) AS avg_points
-  FROM f1_presentation.calculated_race_results
+FROM f1_presentation.calculated_race_results
 GROUP BY driver_name
 HAVING total_races >= 50
 ORDER BY avg_points DESC;
 
 -- COMMAND ----------
 
-SELECT driver_name,
+SELECT 
+       driver_name,
        COUNT(1) AS total_races,
        SUM(calculated_points) AS total_points,
        AVG(calculated_points) AS avg_points
-  FROM f1_presentation.calculated_race_results
+FROM f1_presentation.calculated_race_results
 WHERE race_year BETWEEN 2011 AND 2020
 GROUP BY driver_name
 HAVING total_races >= 50
@@ -22,11 +24,12 @@ ORDER BY avg_points DESC;
 
 -- COMMAND ----------
 
-SELECT driver_name,
+SELECT
+       driver_name,
        COUNT(1) AS total_races,
        SUM(calculated_points) AS total_points,
        AVG(calculated_points) AS avg_points
-  FROM f1_presentation.calculated_race_results
+FROM f1_presentation.calculated_race_results
 WHERE race_year BETWEEN 2001 AND 2020
 GROUP BY driver_name
 HAVING total_races >= 50
